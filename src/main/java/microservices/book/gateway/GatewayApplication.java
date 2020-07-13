2633e71d41a4c9ad935bdcf05dde6c9782f0db8f
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-07-13 16:11:54 +0200

    iping and irule for ribbon load balancer

--- src/main/java/microservices/book/gateway/GatewayApplication.java
+++ src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -2,9 +2,15 @@ package microservices.book.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import microservices.book.gateway.configuration.RibbonConfiguration;
+
 @EnableZuulProxy
+@EnableEurekaClient
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @SpringBootApplication
 public class GatewayApplication {
 
